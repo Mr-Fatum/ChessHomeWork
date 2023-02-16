53e2f0eebac860e5bf0a8ab158060375cf28cd82
Iluchkinn <114167306+Mr-Fatum@users.noreply.github.com>
2023-02-16 15:43:35 +0500

    Update Main.java

--- My-Chess/src/Main.java
+++ My-Chess/src/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("Welcome to CHESS23. Enter \"GUI\" to play on a Graphical User Interface.");
+        System.out.println("Enter \"GUI\" to play on a Graphical User Interface.");
 
         Pieces pieces = new Pieces();
 
